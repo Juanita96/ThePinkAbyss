205ddd43a67bd53a9bdd421171af24c7f70116fe
Juanita96 <juanaklois@gmail.com>
2025-10-02 13:25:53 -0300

    Button start menu finish

--- ThePinkAbyss/Assets/Menu/Script/MenuSystem.cs
+++ ThePinkAbyss/Assets/Menu/Script/MenuSystem.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class MenuSystem : MonoBehaviour
 {
-    public void Start()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        Debug.Log("Menu Loaded");
-    }
     public void Exit()
     {
         Application.Quit();
         Debug.Log("Menu Closed");
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Debug.Log("Menu Loaded");
     }
 }
