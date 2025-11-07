aebab12135457e2adec36e818de884561509ed4e
Alessia Frey <lateacherdemate@gmail.com>
2025-11-07 13:56:15 -0300

    Fix doors and sparkles

--- ThePinkAbyss/Assets/Scripts/Elements/Doors.cs
+++ ThePinkAbyss/Assets/Scripts/Elements/Doors.cs
@@ -1,15 +1,17 @@
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 using UnityEngine.Tilemaps;
 
 public class Doors : MonoBehaviour
 {
-    [Header("Configuración")]
-    [SerializeField] private string torchLightTag = "TorchLight"; 
-    [SerializeField] private float transparentAlpha = 0.4f;       
+    [Header("Configuracion")]
+    [SerializeField] private GameObject requiredLampLight; 
+    [SerializeField] private float transparentAlpha = 0.4f;
 
     private Tilemap tilemap;
     private TilemapCollider2D tilemapCollider;
     private Color originalColor;
+    private bool isOpen = false;
 
     private void Start()
     {
@@ -22,21 +24,31 @@ public class Doors : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag(torchLightTag))
+        
+        if (other.gameObject == requiredLampLight)
         {
-          
-            if (tilemapCollider != null)
-                tilemapCollider.enabled = false;
+            var light2D = other.GetComponent<Light2D>();
 
-            
-            if (tilemap != null)
+            if (light2D != null && light2D.enabled && !isOpen)
             {
-                Color c = originalColor;
-                c.a = transparentAlpha;
-                tilemap.color = c;
+                AbrirPuerta();
             }
         }
     }
 
+    private void AbrirPuerta()
+    {
+        isOpen = true;
 
+        if (tilemapCollider != null)
+            tilemapCollider.enabled = false;
+
+        if (tilemap != null)
+        {
+            Color c = originalColor;
+            c.a = transparentAlpha;
+            tilemap.color = c;
+        }
+
+    }
 }
