34bd9a0557fd1abf341eb5c677a42f1a08a89578
Juanita96 <juanaklois@gmail.com>
2025-11-24 12:43:45 -0300

    Create of Button Exit

---
+++ ThePinkAbyss/Assets/Scripts/UI/ExitGame.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class ExitGame : MonoBehaviour
+{
+    public void GameClose()
+    {
+        Application.Quit();
+    }
+}
